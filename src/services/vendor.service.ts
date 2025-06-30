8f46f5a70822e224dec4af18c97221c2268964a4
Sina Monajemi <smonajemi@mac.home>
2025-06-29 21:37:42 -0400

    x

--- src/services/vendor.service.ts
+++ src/services/vendor.service.ts
@@ -1,9 +1,7 @@
-import { AllowedVendors, DataAssetItem, Policy } from '../types/policy.types';
 import apiClient from '../utils/apiClient';
 
 const fetchAllVendors = async (params: any): Promise<any> => {
   const response = await apiClient.get<any>('/vendors', { params });
-
   const { vendors, stats } = response.data;
   return {
     vendors,
